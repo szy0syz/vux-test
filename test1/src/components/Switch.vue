03075ce5b76b80002f18f98092c2c1663a930199
szy0syz <185505508@qq.com>
2016-12-07 11:41:43 +0800

    init

---
+++ test1/src/components/Switch.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+	    <rater :value=3></rater>
+	</div>
+	<div>
+	    <rater :value=2 :margin="15" active-color="#04BE02"></rater>
+	</div>
+	<div>
+	    <rater :value=5 :max=6 :font-size=15 active-color="#FF9900"></rater>
+	</div>
+</template>
+<style>
+	div {
+    	margin:5px 0;
+	}
+</style>
